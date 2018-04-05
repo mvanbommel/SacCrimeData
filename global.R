b3d24684c7dc6e5cc28dca8fb1f9aed214aa6138
Matthew van Bommel <mvanbommel@users.noreply.github.com>
2018-04-04 19:37:52 -0700

    Cleaned date times

--- global.R
+++ global.R
@@ -2,17 +2,34 @@ library(DT)
 
 dispatch_data = read.csv('Sacramento_Dispatch_Data_From_Current_Year.csv')
 
-received_time = as.POSIXct(paste0(substr(dispatch_data$Received_Date[1], 1, 10), ' ', dispatch_data$Received_Time[1]))
-at_scene_time = as.POSIXct(paste0(substr(dispatch_data$At_Scene_Date[1], 1, 10), ' ', dispatch_data$At_Scene_Time[1]))
-
-
-available_data_index = function(column, type='time') {
-  if (type == 'time') {
-    which(gsub("^\\s+|\\s+$", "", as.character(column)) != '1899-01-01')
-  } else if (type == 'date') {
-    which(!(gsub("^\\s+|\\s+$", "", substr(as.character(column), 1, 10)) %in% c('', '1899-01-01')))
-  }
+available_data_index = function(column_name, dispatch_data, type='Time') {
+  column = dispatch_data[, column_name]
+  which(!(gsub("^\\s+|\\s+$", "", substr(as.character(column), 1, 10)) %in% c('', '1899-01-01')))
 }
+
+time_points = c('Occurence', 'Received', 'Dispatch', 'Enroute', 'At_Scene', 'Clear')
+
+available_time_index = lapply(paste0(time_points, '_Time'), available_data_index, dispatch_data, type='Time')
+available_date_index = lapply(paste0(time_points, '_Date'), available_data_index, dispatch_data, type='Date')
+
+all_times_available = Reduce(intersect, available_time_index)
+all_dates_available = Reduce(intersect, available_date_index)
+
+all_date_times_available = Reduce(intersect, list(all_time_available, all_dates_available))
+
+dispatch_data = dispatch_data[all_date_times_available, ]
+
+convert_to_date_time <- function(date_time_name, dispatch_data) {
+  dispatch_data[, paste0(date_time_name, '_Date_Time')] = as.POSIXct(paste0(substr(dispatch_data[, paste0(date_time_name, '_Date')], 1, 10), ' ', dispatch_data[, paste0(date_time_name, '_Time')]))
+  dispatch_data = dispatch_data[, -which(colnames(dispatch_data) %in% c(paste0(date_time_name, '_Date'), paste0(date_time_name, '_Time')))]
+  dispatch_data
+}
+
+for (name in time_points) {
+  dispatch_data = convert_to_date_time(name, dispatch_data)
+}
+
+
 
 
 
